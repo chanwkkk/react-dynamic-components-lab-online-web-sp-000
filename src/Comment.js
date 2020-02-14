01eef64abbd67d27bc4fa54d8e462dbfd2b741e5
Yingqi Chen <chanwk18@hotmail.com>
2020-02-14 05:27:07 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -4,8 +4,13 @@ import React, { Component } from 'react';
 export default class Comment extends Component{
   render(){
     return(
+<<<<<<< HEAD
       <div className="comment">
         {this.props.commentText}
+=======
+      <div class="comment">
+        {this.prop.commentText}
+>>>>>>> 466be5b115fd28a4cf967844483a831765273781
       </div>
     )
   }
